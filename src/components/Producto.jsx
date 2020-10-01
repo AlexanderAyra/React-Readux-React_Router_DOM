d36b5dd7f6c1b668dc06a7dd1f6407d8f02d7428
Alexander Ayra <alexander.ayras@gmail.com>
2020-10-01 16:12:03 -0500

    Llevando al usuario a Edicion

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import Swal from 'sweetalert2'
 
 // Redux
@@ -10,6 +10,7 @@ const Producto = ({ producto }) => {
   const { nombre, precio, id } = producto
 
   const dispatch = useDispatch()
+  const history = useHistory() // Habilitar history para redicección
 
   // Confirmar si desea eliminarlo
   const confirmaEliminar = (xid) => {
@@ -31,6 +32,11 @@ const Producto = ({ producto }) => {
     })
   }
 
+  // Funcion que redirige de forma programada
+  const redireccionarEdicion = (producto) => {
+    history.push(`/productos/editar/${producto.id}`)
+  }
+
   return (
     <tr>
       <td>{nombre}</td>
@@ -41,12 +47,13 @@ const Producto = ({ producto }) => {
         </span>
       </td>
       <td className='acciones'>
-        <Link
-          to={`/productos/editar/${id}`}
+        <button
+          type='button'
+          onClick={() => redireccionarEdicion(producto)}
           className='btn btn-primary mr-2'
         >
           Editar
-        </Link>
+        </button>
         <button
           type='button'
           className='btn btn-danger'
